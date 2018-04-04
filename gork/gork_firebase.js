b51784961fcff03a9fdcbf197c3f4132e5c0c1b5
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2018-04-03 17:02:50 -0700

    Firebase connection is a go! Also now starting to handle the formatting of the description text coming in.

---
+++ gork/gork_firebase.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDd6E7enzYa9Sn_LvAPGCE3-wdlegw70qo",
+    authDomain: "gcpgames-87fd7.firebaseapp.com",
+    databaseURL: "https://gcpgames-87fd7.firebaseio.com",
+    projectId: "gcpgames-87fd7"
+};
+firebase.initializeApp(config);
